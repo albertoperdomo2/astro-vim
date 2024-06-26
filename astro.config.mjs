7dd3c62e3da01fea9e6d76e899aa1e0383ac63f7
albertoperdomo2 <alberto.perdomo@crisalix.com>
2024-06-26 15:51:42 +0100

    Add Vercel adapter.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
+import vercel from "@astrojs/vercel/serverless";
 
+// https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), mdx()],
+  output: "server",
+  adapter: vercel()
 });
